a44be3ac6de612bb5a7368fce07377816ab7aa6b
Neeschal1 <neeschalpok04@gmail.com>
2026-04-11 19:33:55 +0545

    ft: add a model to store data related to jobs by recruiters

--- server/apps/jobs/admin.py
+++ server/apps/jobs/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models.entities import Job
 
-# Register your models here.
+admin.site.register(Job)
--- server/apps/jobs/models/entities.py
+++ server/apps/jobs/models/entities.py
@@ -1,3 +1,30 @@
 from django.db import models
+from ...accounts.models.entities import Recruiter
 
-# Create your models here.
+JOBTYPE_CHOICE = (
+    ("full_time", "Full Time"),
+    ("part_time", "Part Time"),
+    ("internship", "Internship"),
+    ("remote", "Remote"),
+)
+
+EXPERIENCE_LEVEL_CHOICE = (
+    ("junior", "Junior"), 
+    ("mid", "Mid Level"), 
+    ("senior", "Senior")
+)
+
+class Job(models.Model):
+    recruiter = models.ForeignKey(Recruiter, on_delete=models.CASCADE, related_name="jobs")
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    location = models.CharField(max_length=255)
+    salary = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    job_type = models.CharField(max_length=50, choices=JOBTYPE_CHOICE,)
+    experience_level = models.CharField(max_length=50, choices=EXPERIENCE_LEVEL_CHOICE)
+    posted_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
